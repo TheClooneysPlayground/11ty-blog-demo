8929e33ede78038b901225d9c5b1d967e14b8cb4
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-05-20 23:39:04 +1000

    Add anchors for markdown

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,22 @@
+// nav
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+
+// markdown
+import MarkdownIt from "markdown-it";
+import MarkdownItAnchor from "markdown-it-anchor";
+import slugify from 'slugify';
+
+const md = new MarkdownIt({ html: true })
+.use(MarkdownItAnchor, {
+	permalink: MarkdownItAnchor.permalink.ariaHidden({
+		class: "header-anchor",
+    	placement: 'before'
+	}),
+});
 
 export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+	// Tell 11ty to use our custom Markdown-it
+	eleventyConfig.setLibrary("md", md);
 };
