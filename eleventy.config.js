a3059cc5092b3a6e16145ee24d612f9462a4d798
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-05-23 18:38:00 +1000

    Add `yaml` for data files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,13 @@
-// nav
+// 11ty Nav
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
-// markdown
+// Bake our own Markdown anchors
 import MarkdownIt from "markdown-it";
 import MarkdownItAnchor from "markdown-it-anchor";
 import slugify from 'slugify';
+
+// Use yaml for data
+import yaml from "js-yaml";
 
 const md = new MarkdownIt({ html: true })
 .use(MarkdownItAnchor, {
@@ -15,6 +18,8 @@ const md = new MarkdownIt({ html: true })
 });
 
 export default function (eleventyConfig) {
+	eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
 	// Tell 11ty to use our custom Markdown-it
